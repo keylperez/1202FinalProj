03f6519c416e67096d0d5c2486696b097649efa0
krisyam <kristian.ayam@gmail.com>
2021-05-11 14:37:51 +0800

    Added JS
    changed the div to button for js to work
    
    added onclick funtion in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
 "use strict";
 
-import {}
+function tabShow(tabID){
+    var i, content;
+  
+    content = document.getElementsByClassName("inventoryBar");
+    for (i = 0; i < content.length; i++) {
+      content[i].className = content[i].className.replace(" activeTab", "");
+    }
+    document.getElementById(tabID).className+=" activeTab";
+}
