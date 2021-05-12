50351ef5bf612575da8fff1db1484f5e841b41a9
keylperez <perez.williamcaleb@gmail.com>
2021-05-12 08:55:41 +0800

    fixed changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-import {}
+const gameLabelContainer = document.querySelector(".gameLabelContainer");
+
+
+gameLabelContainer.addEventListener("click", function (e){
+
+
+    var contentID;
+    const selected = e.target.closest(".gameLabel");
+    
+    const content = document.querySelectorAll(".inventoryBar");
+    for (let i = 0; i < content.length; i++) {  
+        content[i].className = content[i].className.replace(" activeTab", ""); //remove active tabs over the .inventoryBars
+    }
+
+    
+    document.querySelector(`.inventoryBar--${selected.dataset.tab}`).classList.add("activeTab");
+}) 
