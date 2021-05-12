3b02e6c82eaebb29c4fc03d3cc829ded9e637b3d
keylperez <perez.williamcaleb@gmail.com>
2021-05-12 10:26:29 +0800

    Background functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,63 @@
 "use strict";
 
 const gameLabelContainer = document.querySelector(".gameLabelContainer");
+const body = document.body;
 
 
 gameLabelContainer.addEventListener("click", function (e){
 
-
-    var contentID;
     const selected = e.target.closest(".gameLabel");
+    if (!selected) return;
+    const dataNum = selected.dataset.tab;
     
     const content = document.querySelectorAll(".inventoryBar");
-    for (let i = 0; i < content.length; i++) {  
-        content[i].className = content[i].className.replace(" activeTab", ""); //remove active tabs over the .inventoryBars
-    }
+    content.forEach((el) => el.classList.remove("activeTab"));
 
     
-    document.querySelector(`.inventoryBar--${selected.dataset.tab}`).classList.add("activeTab");
-}) 
+    document.querySelector(`.inventoryBar--${dataNum}`).className += " activeTab";
+    switch(dataNum){
+        case "1":
+            body.style.backgroundImage = "url('../src/img/bg/warframe-min.png')";
+            console.log("success bg");
+            break;
+        case "2":
+            body.style.backgroundImage = "url('../src/img/bg/honkai-min.png')";
+            console.log("success bg");
+            break;
+        case "3":
+            body.style.backgroundImage = "url('../src/img/bg/dota-min.png')";
+            console.log("success bg");
+            break;
+        case "4":
+            body.style.backgroundImage = "url('../src/img/bg/csgo-min.jpg')";
+            console.log("success bg");
+            break;
+        case "5":
+            body.style.backgroundImage = "url('../src/img/bg/genshin-min.png')";
+            console.log("success bg");
+            break;
+        case "6":
+            body.style.backgroundImage = "url('../src/img/bg/rocketLeague-min.png')";
+            console.log("success bg");
+            break;
+        case "7":
+            body.style.backgroundImage = "url('../src/img/bg/valorant-min.png')";
+            console.log("success bg");
+            break;
+        case "8":
+            body.style.backgroundImage = "url('../src/img/bg/apex-min.jpg')";
+            console.log("success bg");
+            break;
+        case "9":
+            body.style.backgroundImage = "url('../src/img/bg/tf2-min.jpg')";
+            console.log("success bg");
+            break;
+        case "10":
+            body.style.backgroundImage = "url('../src/img/bg/pubg-min.jpg')";
+            console.log("success bg");
+            break;
+        default:
+            console.log("error bg");
+            break;
+    }
+});
