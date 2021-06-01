8653bbeb2c7ce24b58a22f14efc78adf253106d1
keylperez <perez.williamcaleb@gmail.com>
2021-06-01 14:35:29 +0800

    database additions

---
+++ sql_inventory.sql
@@ -0,0 +1,10 @@
+SELECT * FROM sql_inventory.item_inventory;
+
+-- USE sql_inventory;
+	
+-- SELECT
+-- FROM products
+-- WHERE product_id = 1 
+-- ORDER BY name
+-- SELECT product_id, name,game, item_price, item_price * 10 + 100
+-- FROM products 
